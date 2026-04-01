a423af9bd5d38b22af28e6891e48476af9929f17
harshita8120 <harshita.ranjan25@gmail.com>
2026-04-02 01:52:17 +0530

    change

--- bookAPI/settings.py
+++ bookAPI/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,14 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fhr+o_d7ghuqvfc2e!1$_zc^x&^kxh3da2t1n2l4!6ne=5tfss'
+SECRET_KEY = os.environ.get('django-insecure-fhr+o_d7ghuqvfc2e!1$_zc^x&^kxh3da2t1n2l4!6ne=5tfss', 'fallback-key-for-local-dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = [
-    'bookAPIApp.onrender.com', 'localhost', '127.0.0.1'
-]
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+# [
+#     'bookAPIApp.onrender.com', 'localhost', '127.0.0.1'
+# ]
 
 
 # Application definition
